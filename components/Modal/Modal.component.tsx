2f73aeab3c70f71d9c12b1374fe382ddd1399dc4
Ezequiel Sanchez <squiel91@gmail.com>
2022-10-21 15:19:47 -0300

    updated Modal and Input components

--- components/Modal/Modal.component.tsx
+++ components/Modal/Modal.component.tsx
@@ -5,12 +5,14 @@ import styles from './Modal.component.module.scss'
 const Modal: FC<{
   show: boolean,
   compact?: boolean
+  overflowAuto?: boolean,
   children: React.ReactNode,
   onDismiss: () => void
 }> = ({
   show,
   onDismiss,
   compact = false,
+  overflowAuto = true,
   children
 }) => {
   if (!show) {
@@ -33,22 +35,13 @@ const Modal: FC<{
   return (
     <>
       <div className={styles.modalOverlay} onClick={onDismiss}></div>
-      <div className={`${styles.modal} ${compact ? styles.compact : ''}`}>
+      <div className={`
+        ${styles.modal}
+        ${compact ? styles.compact : ''}
+        ${overflowAuto ? styles.overflowAuto : ''}
+      `}>
         <div className={styles.contentWrapper}>
-          <div className={styles.close} onClick={onDismiss}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              fill="currentColor"
-              viewBox="0 0 16 16"
-            >
-              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5
-                0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8
-                8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646
-                5.354a.5.5 0 0 1 0-.708z"/>
-            </svg>
-          </div>
+          <div className={styles.close} onClick={onDismiss}>+</div>
           {children}
         </div>
       </div>
